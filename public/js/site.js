e23d0ebce2b2f2d3b4d7e5091b2cb05050a43705
Michael Duffey <mduffey07@gmail.com>
2017-10-30 03:23:09 -0500

    First commit

---
+++ public/js/site.js
@@ -0,0 +1,13 @@
+function Font(fontName, display) {
+	this.FontName = fontName;
+	this.Display = display;
+}
+
+var fonts = [new Font('janda', 'Janda'), new Font('noregretssolid', "No Regrets"), 
+	new Font('sister', "Love Ya Like a Sister"), new Font('satisfied', "Satisfied"),
+	new Font('apple', "Janda Apple Cobbler"), new Font('elegant', "Janda Elegant"),
+	new Font('stronger', "Makes You Stronger"), new Font('closer', "Closer To Free"),
+	new Font('talking', "Talking to the Moon")];
+
+$(document).ready(function () {
+});
